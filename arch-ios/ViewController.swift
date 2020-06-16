b170472c23a115d74439e6e9901844a5271fe40d
zabochen <zabochen@gmail.com>
2020-06-16 14:42:30 +0300

    Initial Commit

---
+++ arch-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  arch-ios
+//
+//  Created by Taras on 16.06.2020.
+//  Copyright © 2020 Taras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
